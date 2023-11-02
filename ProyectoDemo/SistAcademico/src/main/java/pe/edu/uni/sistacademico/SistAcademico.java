230c0d792e3e10a47fcec6f879b7b1fb1a66875a
Gustavo Coronel <gcoronelc@gmail.com>
2023-11-02 15:57:24 -0500

    Semana 10

--- ProyectoDemo/SistAcademico/src/main/java/pe/edu/uni/sistacademico/SistAcademico.java
+++ ProyectoDemo/SistAcademico/src/main/java/pe/edu/uni/sistacademico/SistAcademico.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package pe.edu.uni.sistacademico;
 
-/**
- *
- * @author LAB M5-E
- */
 public class SistAcademico {
 
     public static void main(String[] args) {
